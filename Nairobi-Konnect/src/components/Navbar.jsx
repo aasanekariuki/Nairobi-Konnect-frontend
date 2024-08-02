094d1662edfd1e1fe975282177bcddd0bc89b9ea
LidyaGhilay <lidyahenok9@gmail.com>
2024-08-03 00:37:41 +0300

    Initialcommit

--- Nairobi-Konnect/src/components/Navbar.jsx
+++ Nairobi-Konnect/src/components/Navbar.jsx
@@ -7,14 +7,20 @@ const Navbar = () => {
         document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
     };
 
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className="navbar-brand">NairobiKonnect</Link>
+                <Link to="/" className="navbar-brand" onClick={scrollToTop}>
+                    NairobiKonnect
+                </Link>
                 <ul className="navbar-menu">
-                    <li><Link to="/" className="navbar-item">Home</Link></li>
-                    <li><Link to="#services" className="navbar-item" onClick={() => handleNavClick('services')}>Services</Link></li>
-                    <li><Link to="#contact" className="navbar-item" onClick={() => handleNavClick('contact')}>Contact</Link></li>
+                    <li><a href="#home" className="navbar-item" onClick={scrollToTop}>Home</a></li>
+                    <li><a href="#services" className="navbar-item" onClick={() => handleNavClick('services')}>Services</a></li>
+                    <li><a href="#contact" className="navbar-item" onClick={() => handleNavClick('contact')}>Contact</a></li>
                     <li><Link to="/signup" className="navbar-item">Signup</Link></li>
                     <li><Link to="/login" className="navbar-item">Login</Link></li>
                 </ul>
