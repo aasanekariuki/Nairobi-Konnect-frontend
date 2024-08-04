898c5d0e470332b51640c75efe509f0c0440942b
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-08-04 08:18:25 +0300

    add images

--- Nairobi-Konnect/src/components/Seller.jsx
+++ Nairobi-Konnect/src/components/Seller.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import axios from 'axios'
 
-function Seller() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const images = [
+  'https://c7.alamy.com/comp/BXN9B4/vendor-in-the-city-market-nairobi-kenya-BXN9B4.jpg',
+  'https://c7.alamy.com/comp/2MBRYY1/anselm-store-at-the-village-market-nairobi-ke-2MBRYY1.jpg',
+  'https://c7.alamy.com/comp/2MBKJNM/bags-with-a-variety-of-nuts-at-carrefour-village-market-nairobi-ke-2MBKJNM.jpg',
+  'https://c7.alamy.com/comp/2MBRYPB/african-honey-display-an-tasting-at-the-carrefour-village-market-nairobi-ke-2MBRYPB.jpg',
+  'https://c7.alamy.com/comp/GFN02P/a-t-shirt-that-reads-reduce-reuse-seen-among-other-clothing-items-GFN02P.jpg'
+]
 
 export default Seller
