8f24738d488b1eb7b1025287b8c5076d19d1b61d
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-08-06 17:53:10 +0300

    insertion of images to drivers page

--- Nairobi-Konnect/src/components/Driver.jsx
+++ Nairobi-Konnect/src/components/Driver.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 
-function Driver() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Driver
+const images = [
+  'https://images.pexels.com/photos/135620/pexels-photo-135620.jpeg?auto=compress&cs=tinysrgb&w=600',
+  'https://images.pexels.com/photos/135620/pexels-photo-135620.jpeg',
+  'https://lh3.googleusercontent.com/UmleRgRVFO1XPWAuoIlmKTcODOx8rKKrPJksb2vejai-8SZzYpraCkLzV-57l3gZIbJ5YKd_NdSroma4kS4K0WoCqoIDTdIDgO4psV5EUtw=s750',
+  'https://coolmen.africa/wp-content/uploads/2024/01/Perfume-Shop.jpg',
+];
