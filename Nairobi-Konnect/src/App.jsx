dcb48d3e8683d2cb9dceb688cd666f4fb85e6986
Maxwel <maxwellbakora@gmail.com>
2024-08-06 19:25:50 +0300

    Finished

--- Nairobi-Konnect/src/App.jsx
+++ Nairobi-Konnect/src/App.jsx
@@ -13,21 +13,19 @@ import User from './components/User';
 function App() {
   return (
     <Router>
-     {/* Navbar will be displayed on every page */}
-      <div className="app-content">
-        <Routes>
-          <Route path="/" element={<Landing />} /> {/* Default route */}
-          <Route path="/bus" element={<Bus />} />
-          <Route path="/driver" element={<Driver />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/seller" element={<Seller />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/stalls" element={<Stalls />} />
-          <Route path="/user" element={<User />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/bus" element={<Bus />} />
+        <Route path="/driver" element={<Driver />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/seller" element={<Seller />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/stalls" element={<Stalls />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
     </Router>
   );
 }
 
-export default App;s
+export default App;
